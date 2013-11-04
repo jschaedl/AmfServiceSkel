73a419db7779c5bb1547caecf1dda6be39b06bf9
Jan Schädlich <schaedlich.jan@gmail.com>
2013-11-04 21:32:18 +0100

    Fixed amfphp bootstrap

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 require_once (dirname(__FILE__)) . '/vendor/autoload.php';
 
 
-$servicePath = dirname(__FILE__) . '/src/Service/AmfphpDoctrineExampleService.php';
-$serviceClassFindInfo = new Amfphp_Core_Common_ClassFindInfo($servicePath, "AmfphpDoctrineExampleService");
+$servicePath = dirname(__FILE__) . '/src/Service/UserService.php';
+$serviceClassFindInfo = new Amfphp_Core_Common_ClassFindInfo($servicePath, "UserService");
 
 $config = new Amfphp_Core_Config();
-$config->pluginsConfig['AmfphpCustomClassConverter'] = array('customClassFolderPaths' => array(dirname(__FILE__) . '/src/Service/Vo/'));
-$config->serviceNames2ClassFindInfo["AmfphpDoctrineExampleService"] = $serviceClassFindInfo;
+$config->pluginsConfig['AmfphpCustomClassConverter'] = array('customClassFolderPaths' => array(dirname(__FILE__) . '/src/Model'));
+$config->serviceNames2ClassFindInfo["UserService"] = $serviceClassFindInfo;
 
 $gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway($config);
 $gateway->service();
